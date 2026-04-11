eb855a80fb6a3b2124f23e11c93bf9c628584a4a
Ricardo Negreiros <75104283+httpmerak@users.noreply.github.com>
2026-04-11 02:04:34 -0300

    feat: add new male and female head assets and textures to cfx_onx_mp_faces

---
+++ cfx_onx_mp_faces/fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'ONX/Ceptar'
+contributors 'Ceptar, GTAWiseguy, JunkyWRX, YoinksOG, Manic'
+description 'New Multiplayer Ped Model Faces'
+version '1.1.0'
+lua54 'yes'
+
+server_scripts {
+  
+}
+
+files {
+}
+
+dependency '/assetpacks'
+
+
+-- Open Source by @kjsdecryption ON DC
+
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC
+--- ADD @kjsdecryption ON DC

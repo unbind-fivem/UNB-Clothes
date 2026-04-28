c813d50ef30fda7afa33a5b0c6a8967175597442
Ricardo Negreiros <75104283+httpmerak@users.noreply.github.com>
2026-04-27 22:37:07 -0300

    feat: add new custom face, skin, and eye asset packs with accompanying manifests

---
+++ [pack]/BetterSkinPack_S1/fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+name 'BetterFaces'
+description 'Face Pack for MP-Peds. Orginaly created by BAHO'
+version 'v1.0'
+author '1iq_'
+
+files {
+  '*.meta'
+}

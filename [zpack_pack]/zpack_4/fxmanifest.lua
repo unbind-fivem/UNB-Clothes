eeb32c56b11d6f00aa86bc54718e853acd2ffa84
Ricardo Negreiros <75104283+httpmerak@users.noreply.github.com>
2026-05-20 23:03:17 -0300

    feat: upload pacote 6

---
+++ [zpack_pack]/zpack_4/fxmanifest.lua
@@ -0,0 +1,19 @@
+-- Generated with Durty Cloth Tool v3.2.0.5 (https://gta.clothing)
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+author 'mendu'
+description 'by zaiders store'
+version '1.0'
+
+files {
+  'mp_m_freemode_01_mp_m_zpack_4_shop.meta',
+  'mp_f_freemode_01_mp_f_zpack_4_shop.meta',
+  'pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_zpack_4_shop.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_zpack_4_shop.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
+dependency '/assetpacks'
